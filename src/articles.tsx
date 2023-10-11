3f2d959d5a4904fc533905d6421c2f62a4d43f81
MicheleM98 <55845560+MicheleM98@users.noreply.github.com>
2023-10-11 09:10:22 +0200

    fix eslint rules

--- src/articles.tsx
+++ src/articles.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useQuery, useMutation } from "@tanstack/react-query";
 import {
   Button,
--- src/users.tsx
+++ src/users.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery, useMutation } from "@tanstack/react-query";
 import {
